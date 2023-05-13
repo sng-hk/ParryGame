ead6e394e70e765004ffc46e75ae624b2c93af53
sng-hk <csh091456743@gmail.com>
2023-05-13 22:36:20 +0900

    0513
    대시 수정

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -43,18 +43,22 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Animator animator;
 
     [Header("Player Input")]
-    Vector2 _moveInput;
+    Vector2 moveInput;
 
     [Header("Events")]
     [Space]
     public UnityEngine.Events.UnityEvent OnLandEvent;
 
-    //+
-    public float dashSpeed = 1f; // 대쉬 속도
-    public float dashDuration = 0.5f; // 대쉬 지속 시간
-    private bool isDashing = false; // 대쉬 중인지 여부를 나타내는 변수
-    private float dashTimer = 0f; // 대쉬 타이머
-    //++
+    [Header("TrailRenderer")]
+    [SerializeField] private TrailRenderer tr;
+
+    [Header("Dash")]
+    private bool canDash = true;
+    private bool isDashing = false;
+    private float dashingPower = 12f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 0.5f;
+    private Vector2 dashDir;
 
     private void Awake()
     {
@@ -64,48 +68,38 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-
+        tr.emitting = false;
     }
-
 
     void Update()
     {
-        _moveInput.x = Input.GetAxisRaw("Horizontal");
-        Velocity = RB.velocity.x;
-
-        if (_moveInput.x != 0)
-        {
-            CheckDirectionToFace(_moveInput.x > 0);
-        }
-
-        //+
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (!isDashing)
-            {
-                isDashing = true;
-                dashTimer = 0f;
-            }
-            else
-            {
-                isDashing = false;
-            }
-        }
-
-        // 대쉬 중인 경우에는 일정 시간동안 일정한 속도로 이동합니다.
         if (isDashing)
         {
-            dashTimer += Time.deltaTime;
-            if (dashTimer < dashDuration)
+            return;
+        }
+
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        Velocity = RB.velocity.x;
+
+        if (moveInput.x != 0)
+        {
+            CheckDirectionToFace(moveInput.x > 0);
+        }
+
+        #region Dash
+        if (Input.GetKeyDown(KeyCode.Space) && canDash)
+        {
+            if(moveInput != Vector2.zero)
             {
-                transform.Translate(Vector3.right * dashSpeed * Time.deltaTime);
+                dashDir = moveInput;
             }
             else
             {
-                isDashing = false;
+                dashDir = isFacingRight ? Vector2.right : Vector2.left;
             }
+            StartCoroutine(nameof(Dash), dashDir);
         }
-        //++
+        #endregion
 
         #region Jump Check
         onGround = Physics2D.OverlapBox(_groundCheckPoint.position, _groundCheckSize, 0, groundLayer);
@@ -113,7 +107,7 @@ public class PlayerController : MonoBehaviour
         // CoyoteTime
         if (onGround)
         {
-            coyoteTimeCounter = coyoteTime;            
+            coyoteTimeCounter = coyoteTime;
         }
         else
         {
@@ -128,18 +122,24 @@ public class PlayerController : MonoBehaviour
         #endregion
 
         #region Animator
-        animator.SetFloat("HorizontalInput", Mathf.Abs(_moveInput.x));
+        animator.SetFloat("HorizontalInput", Mathf.Abs(moveInput.x));
 
         animator.SetFloat("VerticalVel", RB.velocity.y);
         animator.SetBool("OnGround", onGround);
         #endregion
+
     }
 
     private void FixedUpdate()
     {
+        if (isDashing)
+        {
+            return;
+        }
+
         #region Run
         //Calculate the direction we want to move in and our desired velocity
-        float targetSpeed = _moveInput.x * maxSpeed;
+        float targetSpeed = moveInput.x * maxSpeed;
         //We can reduce are control using Lerp() this smooths changes to are direction and speed
         targetSpeed = Mathf.Lerp(RB.velocity.x, targetSpeed, lerpAmount);
 
@@ -158,15 +158,15 @@ public class PlayerController : MonoBehaviour
         RB.AddForce(movement * Vector2.right, ForceMode2D.Force);
 
         /*
-		 * For those interested here is what AddForce() will do
-		 * RB.velocity = new Vector2(RB.velocity.x + (Time.fixedDeltaTime  * speedDif * accelRate) / RB.mass, RB.velocity.y);
-		 * Time.fixedDeltaTime is by default in Unity 0.02 seconds equal to 50 FixedUpdate() calls per second
-		*/
+         * For those interested here is what AddForce() will do
+         * RB.velocity = new Vector2(RB.velocity.x + (Time.fixedDeltaTime  * speedDif * accelRate) / RB.mass, RB.velocity.y);
+         * Time.fixedDeltaTime is by default in Unity 0.02 seconds equal to 50 FixedUpdate() calls per second
+        */
         #endregion
         #endregion
 
         #region Friction
-        if (Mathf.Abs(_moveInput.x) < 0.01f)
+        if (Mathf.Abs(moveInput.x) < 0.01f)
         {
             float amount = Mathf.Min(Mathf.Abs(RB.velocity.x), Mathf.Abs(frictionAmount));
 
@@ -179,7 +179,7 @@ public class PlayerController : MonoBehaviour
         if (jumpTimer > Time.time && coyoteTimeCounter > 0f)
         {
             Jump();
-        }        
+        }
 
         modifyPhysics();
         #endregion
@@ -229,6 +229,27 @@ public class PlayerController : MonoBehaviour
         transform.localScale = scale;
 
         isFacingRight = !isFacingRight;
+    }
+
+    void SetGravityScale(float scale)
+    {
+        RB.gravityScale = scale;
+    }
+
+    private IEnumerator Dash(Vector2 dir)
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = RB.gravityScale;
+        SetGravityScale(0);
+        RB.velocity = dir * dashingPower;
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        SetGravityScale(originalGravity);
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 
     /*private void OnDrawGizmos()
