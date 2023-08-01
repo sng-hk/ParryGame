8dcb211189033cf5f9010a47504a3122631879fa
greenplum916 <66856789+greenplum916@users.noreply.github.com>
2023-08-01 17:40:38 +0900

    적 이동 구현 2
    플레이어를 발견했을 때, 플레이어 방향으로 몸을 돌리고 정지한 채로 총알을 발사함.

--- Assets/Scripts/MovingEnemy.cs
+++ Assets/Scripts/MovingEnemy.cs
@@ -7,15 +7,46 @@ public class MovingEnemy : MonoBehaviour
     public float speed;
     public bool is_left = true;
 
+    public Transform player;
+    public Transform enemy;
+    public float player_x;
+    public float enemy_x;
+
     void Start()
     {
         speed = 2;
+        player = PlayerController.instance.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        player_x = player.position.x;
+        enemy_x = transform.position.x;
+
+        if (EnemySight.recognize == true)
+        {
+            if(enemy_x > player_x)
+            {
+                if (!is_left)
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    is_left = true;
+                }
+            }
+            else if(enemy_x < player_x)
+            {
+                if (is_left)
+                {
+                    transform.eulerAngles = new Vector3(0, 180, 0);
+                    is_left = false;
+                }
+            }
+        }
+        else
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
