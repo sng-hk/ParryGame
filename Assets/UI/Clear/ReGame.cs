7f978aaf7bcdcee9573d0817fc6bc7263eb70c89
greenplum916 <66856789+greenplum916@users.noreply.github.com>
2023-11-06 22:53:10 +0900

    아 맞다
    ㅇㅇ

--- Assets/UI/Clear/ReGame.cs
+++ Assets/UI/Clear/ReGame.cs
@@ -9,10 +9,10 @@ public class ReGame : MonoBehaviour
     public void ClickReGame()
     {
         GoStage.loding_scene = SceneManager.GetActiveScene().name;
-        
+        SceneManager.LoadScene("loading");
         Pause.ResumeGame();
         PlayerController.instance.player_helth_point = PlayerController.instance.player_max_helth_point;
         Resources.UnloadUnusedAssets();
-        SceneManager.LoadScene("loading");
+
     }
 }
