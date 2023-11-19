6710c132783dbc3d2a21c760f7ab817d54acf8d3
greenplum916 <66856789+greenplum916@users.noreply.github.com>
2023-11-19 21:04:55 +0900

    일단 마지막 컷씬 추가 완료

--- Assets/UI/Clear/GoSelectStage.cs
+++ Assets/UI/Clear/GoSelectStage.cs
@@ -17,4 +17,14 @@ public class GoSelectStage : MonoBehaviour
         /*MainMenu.coins += inventory.Coins();
         Resources.UnloadUnusedAssets();*/
     }
+
+    public void GoCutSceneStage()
+    {
+        Pause.ResumeGame();
+        SceneManager.LoadScene("CutScene2");
+        PlayerController.instance.player_helth_point = PlayerController.instance.player_max_helth_point;
+        inventory = GameObject.Find("Player").GetComponent<PlayerInventory>();
+        /*MainMenu.coins += inventory.Coins();
+        Resources.UnloadUnusedAssets();*/
+    }
 }
---
+++ Assets/UI/CutScene/CutSceneUI2.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class CutSceneUI2 : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject setting_object;
+    public Text setting_sound_value_text;
+
+    [SerializeField]
+    private GameObject text_object;
+
+    bool is_on = false;
+
+    public Image cut_scene;
+    public Text script;
+
+    public Sprite[] image;
+    public string[] text;
+
+    [SerializeField]
+    int index = 0;
+    int max_index = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SettingDisable();
+        cut_scene.sprite = image[0];
+        script.text = text[0];
+        index = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            index++;
+            if (index == max_index)
+            {
+                index = 0;
+                GoStageSelectButton();
+            }
+            else
+            {
+                SetIamgeAndText(index);
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(is_on == true)
+            {
+                is_on = false;
+                SettingDisable();
+            }
+            else
+            {
+                is_on = true;
+                SettingEnable();
+            }
+           
+        }
+    }
+
+    public void SettingEnable()
+    {
+        //세팅창 켜기.
+        setting_object.SetActive(true);
+    }
+
+    public void SettingDisable()
+    {
+        //세팅창 끄기.
+        setting_object.SetActive(false);
+    }
+
+    public void SetIamgeAndText(int index)
+    {
+        switch (index)
+        {
+            //검은 화면
+            case 0:
+                cut_scene.sprite = image[0];
+                script.text = text[index];
+                break;
+            //페허
+            case 1:
+                cut_scene.sprite = image[1];
+                script.text = text[index];
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                script.text = text[index];
+                break;
+            //탑을 바라보는 주인공
+            case 7:
+                cut_scene.sprite = image[2];
+                script.text = text[index];
+                break;
+            //무기 빛나는거?
+            case 8:
+                cut_scene.sprite = image[3];
+                text_object.SetActive(false);
+                break;
+            //검은 화면
+            case 9:
+                cut_scene.sprite = image[4];
+                break;
+
+
+
+        }
+    }
+
+    public void GoStageSelectButton()
+    {
+        SceneManager.LoadScene("StageSelect");
+        Resources.UnloadUnusedAssets();
+    }
+
+}
